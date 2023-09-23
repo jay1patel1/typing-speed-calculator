78d3303bbd621015152980bc4946d7cb5ab33025
jay1patel1 <140941387+jay1patel1@users.noreply.github.com>
2023-09-23 20:03:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,94 @@
-/* *****DOM Selection***** */
-let startButton = document.querySelector("#start")
-let stopButton = document.querySelector("#stop")
-let colorPallet = document.querySelector("#colorPallet")
+// * HTML Elements
+let pageHTML = document.getElementById('page')
 
-/* *****Functional Codes***** */
+// * Global Variable
+let keyStrokeCount = 0;
+const keys = `1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKLz:"ZXCVBNM<>?`
+let activeKey = 'A';
+let typeStartTimeStamp;
+let lastPushTimeStamp;
+let typeSpeed;
 
-/* Hex Array */
-let hexCodeContainer = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+// ** Page1 HTML
+const pG_1 = `
+        <div id="title" class="visible">
+            <div class="bg-[#737690] py-10 px-8 ring-[#EAEEFF] rounded-lg shadow-lg shadow-[#EAEEFF]/25">
+            <p class="text-[#EAEEFF] font-extrabold font-mono text-3xl">Press any key to start typing</p>
+            </div>
+        </div>
+`;
+// ** Page2 HTML
+const pG_2 = `
+<div class="bg-[#737690] p-6 rounded-lg shadow-2xl ring-[#EAEEFF] shadow-[#EAEEFF]/25 flex flex-col items-center justify-center">
+        <div class="bg-[#EAEEFF] mb-6 my-auto w-32 h-32 rounded-xl shadow-inner border-solid border-2 border-[#212C3D] flex justify-center items-center">
+            <div class="text-6xl">
+                <p id="randomKey"></p>
+            </div>
+        </div>
+        <div class="mb-6">
+            <table class="border-separate border-spacing-2 border border-[#EAEEFF]">
+                <thead class="bg-[#D2A517] ">
+                <tr>
+                    <th class="border border-[#EAEEFF] w-32">Key Pressed</th>
+                    <th class="border border-slate-300 w-32">Key Code</th>
+                    <th class="border border-slate-300 w-32">Code</th>
+                </tr>
+                </thead>
+                <tbody class="text-gray-50 font-bold text-center h-16 text-xl">
+                <tr>
+                    <td class="border border-slate-300" id="keyPressed"></td>
+                    <td class="border border-slate-300" id="keyCode"></td>
+                    <td class="border border-slate-300" id="code"></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="bg-[#857555] p-5 border-2 rounded-lg mb-6 text-[#FFEECA] text-xl font-bold">
+            <p>Speed: <span id="tp_Speed">0</span> WPM</p>
+        </div>
+        <div class="bg-[#D2A517] p-2 rounded-lg">
+            <p>Press "Esc" to exit</p>
+        </div>
+    </div>
+`;
+// ** Page Control
+// * By Default
+pageHTML.innerHTML = pG_1
 
-/* Function to generate random No */
-const randomNo = function (){
- let a = Math.floor(Math.random() * hexCodeContainer.length);
- return a;
+// * Functions
+// ** Random No Gen
+let randomKey = function () {
+    let randomNo = Math.floor(Math.random() * keys.length);
+    activeKey = `${keys[randomNo]}`
+    document.querySelector('#randomKey').innerHTML = activeKey
 }
-
-/* Function to generate random Color */
-const randomHexCode = function () {
- let b = "#"
- for (let i = 0; i < 6; i++) {
-  b = b + hexCodeContainer[randomNo()];
- }
- console.log(b);
- colorPallet.innerHTML = b;
- colorPallet.style.backgroundColor = b;
- b = null;
-}
-
-/* Function to generate random color continuously */
-let randomHexCodeGenFun;
-let randomHexCodeGen = function () {
- randomHexCodeGenFun = setInterval(randomHexCode,1000)
-}
-
-/* Function to stop generate random color continuously */
-let stopRandomHexCode = function () {
- clearInterval(randomHexCodeGenFun);
-}
-
-
-/* *****Dom Manipulation Code***** */
-
-startButton.addEventListener('click', randomHexCodeGen);
-stopButton.addEventListener('click',stopRandomHexCode);
-
-
-
-
+// ** Button Function
+window.addEventListener("keydown", function (event) {
+    let eventKey = event.key;
+    let eventKeyCode = event["keyCode"];
+    let eventCode = event.code;
+    if (keyStrokeCount === 0) {
+        pageHTML.innerHTML = pG_2
+        typeStartTimeStamp = Date.now();
+        keyStrokeCount += 1
+        randomKey()
+    } else {
+        if (eventKeyCode === 27) {
+            pageHTML.innerHTML = pG_1;
+            keyStrokeCount = 0;
+        } else {
+            document.querySelector("#keyPressed").innerHTML = `${eventKey}`;
+            document.querySelector("#keyCode").innerHTML = `${eventKeyCode}`;
+            document.querySelector("#code").innerHTML = `${eventCode}`;
+            if (eventKey === activeKey) {
+                randomKey();
+                lastPushTimeStamp = Date.now();
+                typeSpeed = Math.floor(keyStrokeCount/((lastPushTimeStamp - typeStartTimeStamp)/60000));
+                document.querySelector('#tp_Speed').innerHTML = typeSpeed
+                keyStrokeCount += 1;
+            }
+        }
+    }
+});
 
 
